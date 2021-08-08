a7a7b03e7eb1204dfb6001f75f04dbad027b3a65
tomartin-42 <tomasmartinezcurz@gmail.com>
2021-08-08 21:20:14 +0200

    pruebas compilar minilib y make

---
+++ includes/graphic.h
@@ -0,0 +1,14 @@
+#ifndef GRAPHIC_H
+# define GRAPHIC_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <math.h>
+# include <time.h>
+
+# include "../minilibx_mms/mlx.h"
+# include "libft.h"
+
+#endif
