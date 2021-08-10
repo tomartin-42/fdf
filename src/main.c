c52906b2ae15bad747ffb211ea7ab0c92fabdd22
Tommy <tommy@Tommys-iMac.local>
2021-08-10 14:49:04 -0700

    primeros pasos

--- src/main.c
+++ src/main.c
@@ -1,15 +1,19 @@
 #include "../includes/graphic.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	void	*mlx;
 	void	*mlx_win;
-	char	*s;
 	
-	s = "HELLO";
-
-	ft_strlen (s);
+	if (argc != 2)
+		exit ();
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_win = mlx_new_window(mlx, 1600, 1200, "Hello world!");
+	mlx_pixel_put(mlx, mlx_win, 80, 80, 0x00FFFFFF);
+	mlx_pixel_put(mlx, mlx_win, 100, 100, 0x00FFFFFF);
 	mlx_loop(mlx);
+}
+
+void	read_map (char **argv)
+{
 }
