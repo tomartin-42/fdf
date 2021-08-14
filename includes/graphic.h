729237384a259e85ae56d85be22d98de4f02bf25
Tommy <tommy@Tommys-iMac.local>
2021-08-15 00:30:06 +0200

    pintando lineas, corregir escala

--- includes/graphic.h
+++ includes/graphic.h
@@ -18,6 +18,19 @@ typedef struct s_map{
 	int	scale;
 }	t_map;
 
+typedef struct s_point{
+	int	x;
+	int	y;
+	int	nx;
+	int	ny;
+}	t_point;
+
+typedef struct s_line{
+	int	x;
+	int 	y;
+	int	D;
+}	t_line;
+
 # define X_MED 360
 # define Y_MED 640	
 
@@ -34,6 +47,9 @@ void	copy_to_map(t_map *map, char *argv);
 void	calculate_scale(t_map *map);
 
 void	paint_point(t_map *map, void *mlx, void *mlx_win);
+void	print_line_low(t_point point, void *mlx, void *mlx_win);
+void	print_line_high(t_point point, void *mlx, void *mlx_win);
+t_point	calculate_points(t_map *map, int x, int y);
 
 void printmap(t_map *map);
 #endif
--- src/paint.c
+++ src/paint.c
@@ -4,8 +4,7 @@ void	paint_point(t_map *map, void *mlx, void *mlx_win)
 {
 	int	i;
 	int	j;
-	int	ini = 200;
-	int	a;
+	t_point	point;
 
 	i = 0;
 	while (i < map->y)
@@ -13,10 +12,98 @@ void	paint_point(t_map *map, void *mlx, void *mlx_win)
 		j = 0;
 		while(j < map->x)
 		{
-			a = (map->xy[i][j] * sin(M_PI / 6)); 
-			mlx_pixel_put(mlx, mlx_win, ini +  (((j - i) * cos (M_PI / 6)) * map->scale) , ini +  (((j + i - a) * sin (M_PI / 6)) * map->scale), 0x00FFFFFF);
+			point = calculate_points(map, i, j);
+			mlx_pixel_put(mlx, mlx_win, point.x, point.y, 0x00FFFFFF);
+			print_line_high(point, mlx, mlx_win);
 			j++;
 		}
 		i++;
 	}
 }
+
+void	print_line_low(t_point point, void *mlx, void *mlx_win)
+{
+	int	dx;
+	int	dy;
+	int	yi;
+	t_line	line;
+	
+	dx = point.nx - point.x;
+	dy = point.ny - point.y;
+	yi = 1;
+	if (dy < 0)
+	{
+		yi = -1;
+		dy = -dy;
+	}
+	line.D = (2 * dy) - dx;
+	line.y = point.y;
+	line.x = point.x;
+	while (line.x <= point.nx)
+	{
+		mlx_pixel_put(mlx, mlx_win, line.x, line.y, 0x00FFFFFF);
+		if (line.D > 0)
+		{
+			line.y = line.y + yi;
+			line.D = line.D + (2 * (dy -dx));
+		}
+		else
+			line.D = line.D + 2 * dy;
+		line.x++;
+	}
+}
+void	print_line_high(t_point point, void *mlx, void *mlx_win)
+{
+	int	dx;
+	int	dy;
+	int	xi;
+	t_line	line;
+	
+	dx = point.nx - point.x;
+	dy = point.ny - point.y;
+	xi = 1;
+	if (dy < 0)
+	{
+		xi = -1;
+		dx = -dx;
+	}
+	line.D = (2 * dx) - dy;
+	line.y = point.y;
+	line.x = point.x;
+	while (line.y <= point.ny)
+	{
+		mlx_pixel_put(mlx, mlx_win, line.x, line.y, 0x00FFFFFF);
+		if (line.D > 0)
+		{
+			line.x = line.x + xi;
+			line.D = line.D + (2 * (dx -dy));
+		}
+		else
+			line.D = line.D + 2 * dx;
+		line.y++;
+	}
+}
+
+/*void	draw_line(t_map *map, t_point point, void *mlx, void *mlx_win)
+{
+}*/
+
+t_point	calculate_points(t_map *map, int x, int y)
+{
+	t_point	point;
+	int	a;
+	int	na;
+
+	a = (map->xy[x][y] * sin(M_PI / 6));
+	point.x = (x - y) * cos (M_PI / 6) * map->scale;
+	point.y = ((x + y - a) * sin (M_PI / 6)) * map->scale;
+	point.x = point.x + 650;
+	point.y = point.y + 100;
+	na = (map->xy[x + 1][y + 1] * sin(M_PI / 6));
+	point.nx = (x + 1 - y + 1) * cos (M_PI / 6) * map->scale;
+	point.ny = ((x + 1 + y + 1 - na) * sin (M_PI / 6)) * map->scale;
+	point.nx = point.x + 650;
+	point.ny = point.y + 100;
+	return (point);
+}
+	
